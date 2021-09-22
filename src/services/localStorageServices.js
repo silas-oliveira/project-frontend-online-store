a71e292063268d785adf3e5e8c9731cbff04ee2f
tiagosathler <sathler@gmail.com>
2021-09-22 14:40:28 -0300

    funções de controle do localStorage prontas

--- src/services/localStorageServices.js
+++ src/services/localStorageServices.js
@@ -1,18 +1,29 @@
+const SHOPPING_CART_KEY = 'shopping_cart';
+
 export function addToCart(obj) {
-  const updArr = [...myArrOfObj, obj];
-  return updArr;
+  const contentLocalStorage = localStorage.getItem(SHOPPING_CART_KEY);
+  const myArrOfObj = JSON.parse(contentLocalStorage);
+  const updArr = obj ? [...myArrOfObj, obj] : myArrOfObj;
+  localStorage.setItem(SHOPPING_CART_KEY, JSON.stringify(updArr));
+  // console.log(JSON.stringify(updArr));
 }
 
 export function subFromCart(obj) {
-  const updArr = myArrOfObj;
-  const indexOjb = myArrOfObj.findIndex((curObj) => curObj.id === obj.id);
-  if (indexOjb >= 0) {
-    removed = updArr.splice(indexOjb, 1);
+  const contentLocalStorage = localStorage.getItem(SHOPPING_CART_KEY);
+  const myArrOfObj = JSON.parse(contentLocalStorage);
+  const indexObj = myArrOfObj.findIndex((curObj) => curObj.id === obj.id);
+  // const updArr = myArrOfObj;
+  if (indexObj >= 0) {
+    // let removed = updArr.splice(indexObj, 1);
+    myArrOfObj.splice(indexObj, 1);
   }
-  return updArr;
+  localStorage.setItem(SHOPPING_CART_KEY, JSON.stringify(updArr));
+  // console.log(JSON.stringify(updArr));
 }
 
 export function sumFromCart(obj) {
+  const contentLocalStorage = localStorage.getItem(SHOPPING_CART_KEY);
+  const myArrOfObj = JSON.parse(contentLocalStorage);
   const count = myArrOfObj.reduce((acc, curObj) => {
     if (curObj.id === obj.id) {
       acc += 1;
@@ -20,4 +31,10 @@ export function sumFromCart(obj) {
     return acc;
   }, 0);
   return count;
+}
+
+export function getFromCart() {
+  const contentLocalStorage = localStorage.getItem(SHOPPING_CART_KEY);
+  const myArrOfObj = JSON.parse(contentLocalStorage);
+  return myArrOfObj;
 }
