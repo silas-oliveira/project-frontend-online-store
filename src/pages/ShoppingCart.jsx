f3129e8c639e3ca84c8a6132515f1e0dbf67944e
Rafael M Friederick <rafael.mf.documents@gmail.com>
2021-09-22 17:31:35 -0300

    Requisito 8 Refatorado

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -5,7 +5,6 @@ import { getFromCart } from '../services/localStorageServices';
 class ShoppingCart extends React.Component {
   constructor() {
     super();
-
     this.state = { contentCart: [] };
     this.saveState = this.saveState.bind(this);
     this.renderContentCart = this.renderContentCart.bind(this);
@@ -24,11 +23,13 @@ class ShoppingCart extends React.Component {
     const { contentCart } = this.state;
 
     const result = contentCart.reduce((formatedArray, current) => {
-      const xavez = formatedArray.findIndex(({ content }) => content.id === current.id);
-      (xavez >= 0)
-        ? formatedArray[xavez].amount += 1
-        : formatedArray.push({ content: current, amount: 1 });
-
+      const maybeIndex = formatedArray
+        .findIndex(({ content }) => content.id === current.id);
+      if (maybeIndex >= 0) {
+        formatedArray[maybeIndex].amount += 1;
+      } else {
+        formatedArray.push({ content: current, amount: 1 });
+      }
       return formatedArray;
     }, []);
     console.log(result);
